0b5ef4b6cda0004c3c876c8a87339c2d3d862680
Süleyman Altan <suleymanaltan.277@gmail.com>
2025-10-30 21:56:50 +0300

    new test update

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -4,11 +4,9 @@ module.exports = {
 		['@babel/preset-react', { runtime: 'automatic' }],
 	],
 	plugins: [
-		['babel-plugin-transform-vite-meta-env', {
-			env: {
-				VITE_API_URL: 'http://localhost:5000',
-				VITE_SOCKET_URL: 'http://localhost:5000'
-			}
+		['babel-plugin-transform-define', {
+			'import.meta.env.VITE_API_URL': '"http://localhost:5000"',
+			'import.meta.env.VITE_SOCKET_URL': '"http://localhost:5000"'
 		}]
 	]
 };
