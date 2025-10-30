dcc07998bf0b38c9dc33633475e0f4f10d902379
Süleyman Altan <suleymanaltan.277@gmail.com>
2025-10-30 21:58:47 +0300

    new test updated

--- frontend/src/__tests__/App.test.jsx
+++ frontend/src/__tests__/App.test.jsx
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from '../App.jsx';
 
 test('renders login page by default', () => {
-	render(
-		<MemoryRouter>
-			<App />
-		</MemoryRouter>
-	);
+	render(<App />);
 	expect(screen.getByText(/Welcome Back/i)).toBeInTheDocument();
 });
 
